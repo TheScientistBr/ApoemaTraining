a453c08350dc9630d2873a3f153288e425b0da75
JotJunior <jot@jot.com.br>
2017-03-31 23:25:26 -0300

    Aplicando exercicios da semana no arquivo aula3.R

--- JotJunior/aula3.R
+++ JotJunior/aula3.R
@@ -1,12 +1,32 @@
-# criando o dataset
-myData <- data.frame(codigo=1:12, mes=month.name, unitario=runif(12), 
-                     quant=rnorm(12,30,5), percentil = abs(rnorm(12))*100, total = 0)
+products <- c("Pneu", "Farol", "Para-choque", "Volante", "Retrovisor", "Buzina", "Bateria", "Extintor", "Para-brisa", "Acendedor", "Radio", "Calota")
+conditions <- c("New", "Damaged", "Refurbished")
+brands <- c("Bosh", "Ford", "Koyo", "PBR")
+
+# Criar um dataset com pelo menos cinco colunas numéricas e três categóricas.
+myData <- data.frame(id=1:12, product=products, condition=sample(conditions,12,replace=T), 
+                     brand=sample(brands,12,replace=T), cost_price=0, 
+                     unit_price=runif(12,1,100), quantity=sample(1:20,12,replace=T), 
+                     total_sales = 0)
+
+
+# aplicando a coluna calculando um preco de custo menor que o preco normal
+myData$cost_price <- myData$unit_price * 0.8 
 
 # aplicando a coluna total com unitario*quant
-myData$total <- myData$quant * myData$unitario # alterando o valor de total
+myData$total_sales <- myData$quantity * myData$unit_price # alterando o valor de total
 
-# criando uma nova coluna total multiplicando unitario por quant (cria uma nova coluna)
-myData <- cbind(myData, total = myData$quant * myData$unitario)
+
+# gravando dataset em disco
+write.csv(myData, '/Users/jot/Cursos/CientistaDeDados/ApoemaTrainig/JotJunior/aula3.csv')
+
+# aplicando a media das colunas de quantidade e total de vendas
+sapply(myData[,5:8], mean )
+sapply(myData[,5:8], sum )
+
+# usando split para separar os produtos por marca
+split(myData, myData$brand)
+
+
 
 myData
 
