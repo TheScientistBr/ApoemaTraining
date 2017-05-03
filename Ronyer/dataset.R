90be3a25f854d7a8dcccb1511872b091d777cf4d
Ronyer <ronyer@gmail.com>
2017-05-02 21:13:50 -0300

    Update dataset.R

--- Ronyer/dataset.R
+++ Ronyer/dataset.R
@@ -4,10 +4,13 @@
 ***myData
 ***mes <- c(ID=1:12,mes=month.name)
 ***tp_Atendimento <- sample(factor(c('u','i')),12,rep=T)
-***medico <- sample(factor(c('Dr.João','Dr.Marcelo')),12,rep=T)
+***medico <- sample(factor(c('Dr.JoÃ£o','Dr.Marcelo')),12,rep=T)
 
 
 
 myData <- data.frame(ID=1:1008,mes=rep(1:12,84),ano=rep(2006:2017,84),idade=rep(0:71,each=14),medico=rep(1:8,126),tp_atendimento=sample(factor(c('u','i')),1008,rep=T),
 tp_paciente=sample(factor(c('p','c')),1008,rep=T),tp_plano=sample(factor(c('amb','pri')),1008,rep=T))
 
+tapply(myData$idade,myData$ano,max)
+tapply(myData$idade,myData$tp_plano,mean)
+summary(myData)
